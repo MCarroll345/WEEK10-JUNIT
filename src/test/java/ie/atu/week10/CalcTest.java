e23e6049d48ba3d7e2c6afd264a871a61191fa91
Mark Carroll <G00419267@atu.ie>
2024-04-11 11:44:05 +0100

    Tests for each action made and passed

--- src/test/java/ie/atu/week10/CalcTest.java
+++ src/test/java/ie/atu/week10/CalcTest.java
@@ -1,9 +1,34 @@
 package ie.atu.week10;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CalcTest {
     Calc newCalc;
 
-    void testAdd(){
-        int result = newCalc.add(4,6);
+    @Test
+    void testAdd()
+    {
+        newCalc = new Calc();
+        assertEquals(10,newCalc.add(4,6));
+    }
+    @Test
+    void testSubract()
+    {
+        newCalc = new Calc();
+        assertEquals(2,newCalc.subtract(6,4));
+    }
+    @Test
+    void testMultiply()
+    {
+        newCalc = new Calc();
+        assertEquals(24,newCalc.multiply(4,6));
+    }
+    @Test
+    void testDivide()
+    {
+        newCalc = new Calc();
+        assertEquals(2,newCalc.divide(8,4));
     }
 }
