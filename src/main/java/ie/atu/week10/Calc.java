247c5b3b86cb1151f9cdabba8193644670a51198
Mark Carroll <G00419267@atu.ie>
2024-04-11 11:31:42 +0100

    Lab started

---
+++ src/main/java/ie/atu/week10/Calc.java
@@ -0,0 +1,9 @@
+package ie.atu.week10;
+
+public class Calc {
+
+    public int add(int firstNum, int secondNum) {
+        int result = firstNum + secondNum;
+        return result;
+    }
+}
---
+++ src/test/java/ie/atu/week10/CalcTest.java
@@ -0,0 +1,9 @@
+package ie.atu.week10;
+
+public class CalcTest {
+    Calc newCalc;
+
+    void testAdd(){
+        int result = newCalc.add(4,6);
+    }
+}
